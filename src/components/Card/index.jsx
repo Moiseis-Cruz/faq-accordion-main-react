6de7534a4397a3aa8870ba2cec2d66784897fd4d
Moiseis Cruz <moizzito@gmail.com>
2024-04-02 12:36:27 -0300

    I created the CardContainer in the Card component function.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,17 +5,23 @@ import ImgStar from "../../images/icon-star.svg"
 export const Card = ({children}) => {
     return(
         <Container>
-            <div>
+            <CardContent>
                 <img src={ImgStar} alt="Icon Star" />
                 <Title />
-            </div>
+            </CardContent>
             {children}
         </Container>
     )
 }
 
 const Container = styled.div`
-    width: 100%;
     max-width: 992px;
+    width: 100%;
+    height: 626px;
     background-color: aliceblue;
+`
+
+const CardContent = styled.div`
+    display: flex;
+    gap: 21px;
 `
