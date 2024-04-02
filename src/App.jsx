c599ae3252225fcb743e1d9ef0aef030f5f096ea
Moiseis Cruz <moizzito@gmail.com>
2024-04-01 21:46:04 -0300

    Removes the div with Title and image from the App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { Card } from "./components/Card";
-import { Title } from "./components/Title";
-
-import ImgStar from "./images/icon-star.svg"
 
 function App() {
   return (
     <>
-      <Card>
-        <div>
-          <img src={ImgStar} alt="" />
-          <Title />
-        </div>
-      </Card>
+      <Card/>
     </>
   );
 }
