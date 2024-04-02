6f0a42f8b80c09b1feb0e6c0c590bc87e89307e8
Moiseis Cruz <moizzito@gmail.com>
2024-04-02 12:35:47 -0300

    I added a Container and styles to it. In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import styled from "styled-components";
 import { Card } from "./components/Card";
 
 function App() {
   return (
-    <>
+    <Container>
       <Card/>
-    </>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.section`
+  width: 100%;
+  max-width: 1440px;
+  min-height: 100vh;
+  background-color: grey;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
