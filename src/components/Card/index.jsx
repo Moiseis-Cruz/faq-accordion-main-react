e3339fc4047137692efe63a8b9cc880c6212c776
Moiseis Cruz <moizzito@gmail.com>
2024-04-03 19:06:08 -0300

    I created the tabs and added the components to them.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import { Title } from "../Title"
 import ImgStar from "../../images/icon-star.svg"
 import { GuideSection } from "../GuideSection"
+import { Question } from "../Question"
+import { QuestionAnswer } from "../QuestionAnswer"
 
 export const Card = () => {
     return(
@@ -12,9 +14,30 @@ export const Card = () => {
             </CardContent>
 
             <ul>
-                <GuideSection>Título 1</GuideSection>
-                <GuideSection>Título 2</GuideSection>
-                <GuideSection>Título 3</GuideSection>
+                <GuideSection>
+                    <Question>What is Frontend Mentor, and how will it help me?</Question>
+
+                    <QuestionAnswer>Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.</QuestionAnswer>
+                </GuideSection>
+
+                <GuideSection>
+                    <Question>Is Frontend Mentor free?</Question>
+
+                    <QuestionAnswer>Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.</QuestionAnswer>
+                </GuideSection>
+
+                <GuideSection>
+                    <Question>Can I use Frontend Mentor projects in my portfolio?</Question>
+
+                    <QuestionAnswer>Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!</QuestionAnswer>
+                </GuideSection>
+
+                <GuideSection>
+                    <Question>How can I get help if I'm stuck on a Frontend Mentor challenge?</Question>
+
+                    <QuestionAnswer>The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.</QuestionAnswer>
+                </GuideSection>
+
             </ul>
         </Container>
     )
