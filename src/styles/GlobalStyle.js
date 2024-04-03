41aee565690cb6200ca837c83929691356323842
Moiseis Cruz <moizzito@gmail.com>
2024-04-03 12:38:35 -0300

    Continuing the Container style of the Card function.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -8,20 +8,15 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', */
-        /* 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', */
-        /* sans-serif; */
-        /* -webkit-font-smoothing: antialiased; */
-        /* -moz-osx-font-smoothing: grayscale; */
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
         background-color: #282c34;
-        /* min-height: 100vh; */
-        /* display: flex; */
-        /* flex-direction: column; */
-        /* align-items: center; */
-        /* justify-content: center; */
-        /* font-size: calc(10px + 2vmin); */
-        /* color: white; */
-        /* width: 100%; */
+        font-size: calc(10px + 2vmin);
+        width: 100%;
         max-width: 1440px;
+        margin: auto;
     }
 `
