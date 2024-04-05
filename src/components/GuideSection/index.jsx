b61cd0ce8dfb27d909060ae0a136d219af63efd7
Moiseis Cruz <moizzito@gmail.com>
2024-04-04 22:09:50 -0300

    I styled some GuideSection properties.

--- src/components/GuideSection/index.jsx
+++ src/components/GuideSection/index.jsx
@@ -1,5 +1,7 @@
+import { hover } from "@testing-library/user-event/dist/hover"
+
 export const GuideSection = (props) => {
     return(
-        <li {...props} />
+        <li style={{}} {...props} />
     )
 }
