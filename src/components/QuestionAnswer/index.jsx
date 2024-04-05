e9d0ef5400d09fbaa6ec31e844af9520a43c7f93
Moiseis Cruz <moizzito@gmail.com>
2024-04-04 22:09:12 -0300

    I used some properties from Question Answer.

--- src/components/QuestionAnswer/index.jsx
+++ src/components/QuestionAnswer/index.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components"
+
 export const QuestionAnswer = (props) => {
     return(
-        <p {...props} />
+        <Description {...props} />
     )
 }
+
+const Description = styled.p`
+    font-size: 16px;
+    color: hsl(292, 16%, 49%);
+    margin: 24px 0;
+`
