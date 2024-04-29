d40d023eda90f31567795fd80f5d40a7fca800fa
Moiseis Cruz <moizzito@gmail.com>
2024-04-29 12:24:59 -0300

    I added Container and CardContent styling to Estilos.js

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -2,6 +2,26 @@ import styled from "styled-components";
 import BgPlus  from "../images/icon-plus.svg";
 import BgMinus from "../images/icon-minus.svg";
 
+export const Container = styled.div`
+    max-width: 600px;
+    width: 100%;
+    min-height: 490px;
+    background-color: hsl(0, 0%, 100%);
+    padding: 46px 36px;
+    overflow: hidden;
+    border-radius: 12px;
+    @media (max-width: 425px) {
+        width: 327px;
+        padding: 23px 18px;
+    }
+`
+
+export const CardContent = styled.div`
+    display: flex;
+    gap: 21px;
+    margin-bottom: 28px;
+`
+
 export const FAQTitle = styled.h1`
     color: hsl(292, 42%, 14%);
     font-size: 56px;
