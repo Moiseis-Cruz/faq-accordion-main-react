0e0b103e6e64ce6fd0094286dd3e1bd1bf05c315
Moiseis Cruz <moizzito@gmail.com>
2024-05-01 16:36:55 -0300

    I added MainContainer styling to Styles.

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 import BgPlus  from "../images/icon-plus.svg";
 import BgMinus from "../images/icon-minus.svg";
+import ImgBgDesktop from "../images/background-pattern-desktop.svg";
+import ImgBgMobile from "../images/background-pattern-mobile.svg";
+
+export const MainContainer = styled.section`
+    width: 100%;
+    max-width: 1440px;
+    min-height: 100dvh;
+    background: top center / contain no-repeat #e5c6f2 url(${ImgBgDesktop});
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* !!!  Observar se o padding é necessário  !!! */
+    padding: 20px;
+    @media (max-width: 375px) {
+        background: top center / contain no-repeat #e5c6f2 url(${ImgBgMobile});
+        padding: 0;
+    }
+`
 
 export const Container = styled.div`
     max-width: 600px;
