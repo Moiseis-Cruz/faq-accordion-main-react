dbb55baf880e59f085c087832fc235623d80c8ea
Moiseis Cruz <moizzito@gmail.com>
2024-05-01 16:25:41 -0300

    I changed the background color of the body.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
         font-family: -apple-system, "Work Sans", sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        background-color: hsl(0, 0%, 100%);
+        background-color: #888888;
         font-size: calc(10px + 2vmin);
         width: 100%;
         max-width: 1440px;
