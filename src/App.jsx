5df3ca705a18e2cd88e56a46f37b34ae9eec1728
Moiseis Cruz <moizzito@gmail.com>
2024-05-01 16:36:01 -0300

    I removed the MainContainer styling and left its import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import styled from "styled-components";
 import { Card } from "./components/Card";
-import ImgBgDesktop from "./images/background-pattern-desktop.svg";
-import ImgBgMobile from "./images/background-pattern-mobile.svg"
+import { MainContainer } from "./styles/Styles";
 
 function App() {
   return (
@@ -12,19 +10,3 @@ function App() {
 };
 
 export default App;
-
-const MainContainer = styled.section`
-  width: 100%;
-  max-width: 1440px;
-  min-height: 100dvh;
-  background: top center / contain no-repeat #e5c6f2 url(${ImgBgDesktop});
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* !!!  Observar se o padding é necessário  !!! */
-  padding: 20px;
-  @media (max-width: 375px) {
-    background: top center / contain no-repeat #e5c6f2 url(${ImgBgMobile});
-    padding: 0;
-  }
-`
